582ef6c9654a859d8bf4e09cbc1d036a6a21fb82
Farhanasharna2000 <farhanasharna22@gmail.com>
2024-10-25 02:52:49 +0600

    added banner component

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -4,11 +4,14 @@ import './Banner.css'
 const Banner = () => {
     return (
         <div style={
-            {backgroundImage:'url(/src/assets/bg-shadow.png)'}} className="flex flex-col items-center space-y-4 bg-cover bg-no-repeat bg-black p-10 container mx-auto rounded-xl mt-2 mb-2">
+            {backgroundImage:'url(/src/assets/bg-shadow.png)'}} className="flex flex-col items-center space-y-4 bg-cover bg-no-repeat bg-black p-10 container mx-auto rounded-xl mt-2 mb-2 ">
             <img className="w-44" src={banner} alt="" />
-            <h1 className="text-3xl text-white font-bold">Assemble Your Ultimate Dream 11 Cricket Team</h1>
+            <h1 className="md:text-3xl text-lg text-center text-white font-bold">Assemble Your Ultimate Dream 11 Cricket Team</h1>
             <p className="text-gray-400">Beyond Boundaries Beyond Limits</p>
-            <button className="btn bg-[#E7FE29] ">Claim Free Credit</button>
+            <div className="border border-[#E7FE29] rounded-xl">
+            <button className="btn bg-[#E7FE29] m-2 ">Claim Free Credit</button>
+
+            </div>
         </div>
     );
 };
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,37 +1,63 @@
 import './Footer.css'
+import footer from "../../assets/logo-footer.png";
 const Footer = () => {
     return (
-        <footer className="footer bg-black text-base-content p-10">
-        <nav>
-          <h6 className=" text-white">About Us</h6>
-          <p className="text-gray-500 w-2/3">We are a passionate team dedicated to providing the best services to our customers.</p>
-        </nav>
-        <nav >
-          <h6 className="text-white">Quick Links</h6>
-          <ul className="list-disc ml-5 text-gray-600">
-            <li><a className="link link-hover">Home</a></li>
-            <li><a className="link link-hover">Services</a></li>
-            <li><a className="link link-hover">About</a></li>
-            <li><a className="link link-hover">Contact</a></li>
-          </ul>
-        </nav>
-        <form className='border-b-gray-700'>
-    <h6 className="text-white">Subscribe</h6>
-    <fieldset className="form-control w-80">
-      <label className="label">
-        <span className="label-text text-gray-600 ">Subscribe to our newsletter for the latest updates.</span>
-      </label>
-      <div className="join">
-        <input
-          type="text"
-          placeholder="Enter your email"
-          className="input input-bordered join-item" />
-        <button className="btn  join-item btn-shadow">Subscribe</button>
-      </div>
-    </fieldset>
-  </form>
-  <p></p>
-      </footer>
+
+        <footer className=" bg-black pt-36  md:pt-40 pb-6 relative md:mt-60 mt-40">
+            <div style={
+                { backgroundImage: 'url(/src/assets/bg-shadow.png)' }} className='absolute z-1 md:top-[-25%] top-[-10%] md:left-[7.5%] left-[4%] bg-white mx-auto pl-4 pr-4 pt-8 pb-8 md:pl-96 md:pr-96 text-center bg-cover bg-no-repeat rounded-xl md:pt-20 md:pb-20'>
+                <form className='border-b-gray-700 '>
+                    <h6 className="md:text-xl font-bold">Subscribe to our Newsletter</h6>
+                    <fieldset className="form-control ">
+                        <label className="label">
+                            <span className="label-text text-gray-600 text-sm">Get the latest updates and news right in your inbox!</span>
+                        </label>
+                        <div className="flex gap-2">
+                            <input
+                                type="text"
+                                placeholder="Enter your email"
+                                className="input input-bordered join-item" />
+                            <button className="btn  btn-shadow ">Subscribe</button>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+            <div className='border-b-2 border-gray-900'>
+                <img className=' mx-auto mb-10' src={footer} alt="" />
+
+                <div className=' md:flex justify-between container mx-auto mb-10 pl-4 md:pl-0'>
+                    <nav className='mb-4 md:mb-0'>
+                        <h6 className=" text-white mb-2">About Us</h6>
+                        <p className="text-gray-500 ">We are a passionate team <br /> dedicated to providing the best <br /> services to our customers.</p>
+                    </nav>
+                    <nav className='mb-4 md:mb-0'>
+                        <h6 className="text-white mb-2">Quick Links</h6>
+                        <ul className="list-disc ml-5 text-gray-600">
+                            <li><a className="link link-hover">Home</a></li>
+                            <li><a className="link link-hover">Services</a></li>
+                            <li><a className="link link-hover">About</a></li>
+                            <li><a className="link link-hover">Contact</a></li>
+                        </ul>
+                    </nav>
+                    <form className='border-b-gray-700 '>
+                        <h6 className="text-white">Subscribe</h6>
+                        <fieldset className="form-control   md:w-80">
+                            <label className="label">
+                                <span className="label-text text-gray-600 ">Subscribe to our newsletter for the latest updates.</span>
+                            </label>
+                            <div className="join">
+                                <input
+                                    type="text"
+                                    placeholder="Enter your email"
+                                    className="input input-bordered join-item" />
+                                <button className="btn  join-item btn-shadow">Subscribe</button>
+                            </div>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+            <p className='text-center mt-6'>@2024 Your Company All Rights Reserved.</p>
+        </footer>
     );
 };
 
